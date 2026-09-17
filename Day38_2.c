739256f3f41a510b4917e1e3fdd468ef54e955d1
Shrish <139778785+Shrish89@users.noreply.github.com>
2026-09-18 00:31:07 +0530

    Add files via upload

---
+++ Day38_2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[10][10];
+    int n, i, j;
+    int symmetric = 1;
+
+    printf("Enter the order of the matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (matrix[i][j] != matrix[j][i])
+            {
+                symmetric = 0;
+                break;
+            }
+        }
+
+        if (symmetric == 0)
+        {
+            break;
+        }
+    }
+
+    if (symmetric == 1)
+    {
+        printf("The matrix is symmetric.\n");
+    }
+    else
+    {
+        printf("The matrix is not symmetric.\n");
+    }
+
+    return 0;
+}
