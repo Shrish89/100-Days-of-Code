56788425fab70951ae9f8291b1d12d31b790cb18
Shrish <139778785+Shrish89@users.noreply.github.com>
2026-09-24 00:13:57 +0530

    Add files via upload

---
+++ Day46_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+            str[i] != 'o' && str[i] != 'u' &&
+            str[i] != 'A' && str[i] != 'E' && str[i] != 'I' &&
+            str[i] != 'O' && str[i] != 'U')
+        {
+            str[j] = str[i];
+            j++;
+        }
+
+        i++;
+    }
+
+    str[j] = '\0';
+
+    printf("String after removing vowels: %s", str);
+
+    return 0;
+}
